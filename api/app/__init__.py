94abc6cb9477e295b1b4293a91ddb526f94fec14
Scott Guthart <seguthart@gmail.com>
2025-03-17 09:59:08 -0400

    remove unneeeded email logging

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,7 +2,7 @@
 from config import Config
 from flask import Flask
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 import os
 from flask_marshmallow import Marshmallow
 from apifairy import APIFairy
@@ -41,26 +41,6 @@ def create_app(config_class=Config):
     app.register_blueprint(api_bp, url_prefix="/api")
 
     if not app.debug:
-        email_auth = None
-        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-            email_auth = (
-                app.config["MAIL_USERNAME"],
-                app.config["MAIL_PASSWORD"],
-            )
-        secure = None
-        if app.config["MAIL_USE_TLS"]:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
-            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
-            toaddrs=app.config["ADMINS"],
-            subject="Responsiv Failure",
-            credentials=email_auth,
-            secure=secure,
-        )
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-
         if not os.path.exists("logs"):
             os.mkdir("logs")
         file_handler = RotatingFileHandler(
